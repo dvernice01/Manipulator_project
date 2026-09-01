7b5583875ac2dbb6a882639d62ebf186b02e7c56
dvernice01 <studenti@DESKTOP-GDG59ML.>
2026-09-01 14:08:27 +0200

    commands to link controller to robot added

--- my_workspace/src/ros2_control_demos/example_7/controller/r6bot_controller.cpp
+++ my_workspace/src/ros2_control_demos/example_7/controller/r6bot_controller.cpp
@@ -30,11 +30,11 @@ using config_type = controller_interface::interface_configuration_type;
 namespace ros2_control_demo_example_7
 {
 RobotController::RobotController() : controller_interface::ControllerInterface() {
-  // AGGIUNTO DA ME 
-  // ---------------------------------------------------------------------
+  // AGGIUNTO DA ME
+  // ---------------------------------------------------------------------
   command_interface_map_["position"] = &joint_position_command_interface_;
   command_interface_map_["velocity"] = &joint_velocity_command_interface_;
-  // ---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
 }
 
 controller_interface::CallbackReturn RobotController::on_init()
